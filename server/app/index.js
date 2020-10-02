2fe17c334c28534b7513882d6fb662d7851fc062
Vinicius <vini.vieir@gmail.com>
2020-10-02 19:35:41 +0000

    server up

---
+++ server/app/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const routes = require('../routes')
+
+class AppController {
+  constructor() {
+    this.express = express()
+
+    this.middlewares()
+    this.routes()
+  }
+
+  middlewares() {
+    this.express.use(express.json())
+    this.express.use(express.urlencoded({ extended: false }))
+    this.express.use(helmet())
+    this.express.use(morgan('dev'))
+    this.express.use(express.static(__dirname + '/public'))
+  }
+
+  routes() {
+    this.express.use(routes)
+  }
+}
+
+module.exports = new AppController().express
