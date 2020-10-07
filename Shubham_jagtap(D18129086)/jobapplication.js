ea70a5620178be7636c29137b884481f36aadfc5
sunnyjagtap23 <71843096+sunnyjagtap23@users.noreply.github.com>
2020-10-07 19:59:00 +0100

    Delete jobapplication.js

--- Shubham_jagtap(D18129086)/jobapplication.js
+++
@@ -1,9 +0,0 @@
-function JobApplicant() {
-    this.processApplication = (jobApplicant) => {
-        if (jobApplicant.experienceInYears >= 3 && jobApplicant.industry === "hospitality" && jobApplicant.bachelorsDegree === true) {
-            return "HIRE";
-        }
-    }
-}
-module.exports = new JobApplicant();
-
