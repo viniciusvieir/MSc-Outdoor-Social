26bb56e8745dbd51da9c7df2d1711052050a4792
neels198 <neel.salvi@gmail.com>
2020-10-12 18:19:55 +0100

    Small prototyping cahnges, added axios

--- client/trailseek/src/screens/SearchTrailScreen.js
+++ client/trailseek/src/screens/SearchTrailScreen.js
@@ -6,14 +6,16 @@ const SearchTrailScreen = ({ navigation}) => {
     return(
         <>
             <Text h3>SearchTrailScreen</Text>
-            <SearchBar />
+            <SearchBar 
+                onChangeText={()=>{navigation.navigate('ListTrail')}}
+            />
             <ScrollView horizontal style={{maxHeight:200}}>
                 <Card containerStyle={{width:300}}>
-                    <Card.Title>HELLO WORLD</Card.Title>
+                    <Card.Title>Trail 1</Card.Title>
                     <Card.Divider/>
                     {/* <Card.Image source={require('../images/pic2.jpg')} /> */}
                     <Text style={{marginBottom: 10}}>
-                        The idea with React Native Elements is more about component structure than actual design.
+                        Trail description Trail description Trail description Trail description Trail description
                     </Text>
                     <Button
                         onPress={()=>navigation.navigate('ViewTrail')}
@@ -22,25 +24,27 @@ const SearchTrailScreen = ({ navigation}) => {
                         title='VIEW NOW' />
                 </Card>
                 <Card containerStyle={{width:300}}>
-                    <Card.Title>HELLO WORLD</Card.Title>
+                    <Card.Title>Trail 2</Card.Title>
                     <Card.Divider/>
                     {/* <Card.Image source={require('../images/pic2.jpg')} /> */}
                     <Text style={{marginBottom: 10}}>
-                        The idea with React Native Elements is more about component structure than actual design.
+                         description Trail description Trail description Trail description Trail description
                     </Text>
                     <Button
+                        onPress={()=>navigation.navigate('ViewTrail')}
                         // icon={<Icon name='code' color='#ffffff' />}
                         // buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
                         title='VIEW NOW' />
                 </Card>
                 <Card containerStyle={{width:300}}>
-                    <Card.Title>HELLO WORLD</Card.Title>
+                    <Card.Title>Trail 2</Card.Title>
                     <Card.Divider/>
                     {/* <Card.Image source={require('../images/pic2.jpg')} /> */}
                     <Text style={{marginBottom: 10}}>
-                        The idea with React Native Elements is more about component structure than actual design.
+                        Trail description Trail description Trail description Trail description Trail description
                     </Text>
                     <Button
+                        onPress={()=>navigation.navigate('ViewTrail')}
                         // icon={<Icon name='code' color='#ffffff' />}
                         // buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
                         title='VIEW NOW' />
