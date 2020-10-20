e2b38eb284ab1366b830a03ac7a7207640219875
Vinicius <vini.vieir@gmail.com>
2020-10-20 19:54:02 +0100

    fixed bug on CI

--- server/src/utils/error-handling.js
+++ server/src/utils/error-handling.js
@@ -2,7 +2,7 @@ const errorHandler = (errors) => {
   const errorsArray = errors.map((error) => {
     return { msg: error }
   })
-  return { errors: errorsArray }
+  return { success: false, errors: errorsArray }
 }
 
 module.exports = { errorHandler }
