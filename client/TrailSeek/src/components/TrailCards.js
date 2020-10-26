b40d19e91cb28e0a3694ca196770c9295f875378
irfan <d19123604@mytudublin.ie>
2020-10-26 20:25:00 +0000

    Chnages in trailcard.js file

--- client/TrailSeek/src/components/TrailCards.js
+++ client/TrailSeek/src/components/TrailCards.js
@@ -6,7 +6,6 @@ import { useNavigation } from '@react-navigation/native';
 import StarRating from 'react-native-star-rating';
 import { AntDesign } from '@expo/vector-icons';
 import * as Location from 'expo-location';
-import Toast from 'react-native-simple-toast';
 
 
 import trailSeek from '../api/trailSeek'
@@ -26,7 +25,6 @@ const TrailCards =  ({getParams,gps}) =>{
         let { status } = await Location.requestPermissionsAsync();
         if (status !== 'granted') {
             setErrorMsg('Permission to access location was denied');
-            Toast.show(errorMsg,Toast.LONG);
         }
         let location1 = await Location.getCurrentPositionAsync({});
         setLocation(location1);
@@ -63,7 +61,6 @@ const TrailCards =  ({getParams,gps}) =>{
                 setData(response.data);
             }
             catch (error) {
-                Toast.show(error,Toast.LONG);
                 console.log(error);
             };
         }
@@ -95,7 +92,7 @@ const TrailCards =  ({getParams,gps}) =>{
     const trails = useSelector(state=>Intersect(state.trails.trails,data))
 
     return (
-        <View>
+        <View style={styles.card}>
             <Text h4 style={styles.titleStyle}>{title}</Text>
             <ScrollView 
                 horizontal 
@@ -113,7 +110,7 @@ const TrailCards =  ({getParams,gps}) =>{
                         <TouchableOpacity
                             onPress={()=>{navigation.navigate('ViewTrail',{id:item._id, name: item.name})}}      
                         >
-                            <View style={{flexShrink: 1}}>
+                            <View style={styles.listitem}>
                             <Image
                                 source={{ uri: item.img_url }}
                                 style={styles.imageStyle}
@@ -146,7 +143,7 @@ const TrailCards =  ({getParams,gps}) =>{
             { trails.length>=10?
                 <TouchableOpacity 
                         style={styles.viewMore}
-                        onPress={()=>{navigation.navigate('ListTrail',{query:query})}}
+                        onPress={()=>{navigation.navigate('ListTrail',{getParams})}}
                     >
                         <AntDesign name="right" size={24} color="black" style={{alignSelf:'center'}}/>
                         <Text style={{fontSize:10}}>View More</Text>
@@ -160,6 +157,20 @@ const TrailCards =  ({getParams,gps}) =>{
 };
 
 const styles = StyleSheet.create({
+    listitem:{
+        flexShrink: 1,
+        borderRadius:5,
+        marginBottom:5,
+        marginTop:5,
+        
+    },
+    card:{
+        backgroundColor:'#fff',
+        marginTop:8,
+        borderRadius:8,
+        
+        
+    },
     imageStyle:{
         borderRadius:3,
         width:250,
@@ -171,18 +182,21 @@ const styles = StyleSheet.create({
         fontWeight:"800",
         fontSize:18,
         flexShrink:1,
-        marginLeft:5
+        marginLeft:5,
+        color:'#404040',
     },
     locationStyle:{
         marginLeft:5,
-        width:160
+        width:160,
+        color:'#666666',
     },
     titleStyle:{
-        marginLeft:10
+        marginLeft:10,
+        color:'#395693',
     },
     caption:{
         width:250,
-        marginLeft:5
+        marginLeft:5,
     },
     rateloc:{
         flexDirection: 'row',
