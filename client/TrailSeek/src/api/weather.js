04ede0a42d58d63762e233a9a74f6d791a6b5e92
NeelSalvi <56079397+NeelSalvi@users.noreply.github.com>
2020-11-05 21:56:58 +0000

    Update weather.js
    Removed Api key

--- client/TrailSeek/src/api/weather.js
+++ client/TrailSeek/src/api/weather.js
@@ -3,7 +3,6 @@ import axiso from "axios";
 export default axiso.create({
   baseURL: "https://api.openweathermap.org/data/2.5",
   params: {
-    appid: "b683b592473451521505990ef238f1f0",
     units: "metric",
   },
 });
