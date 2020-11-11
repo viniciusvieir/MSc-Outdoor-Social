d0fb2a903f7c5557bb2b0ffb88dcc91f698367e6
neels198 <neel.salvi@gmail.com>
2020-11-11 20:05:03 +0000

    Added Event List
    Co-Authored-By: Vinicius Vieira <vini.vieir@gmail.com>

--- client/TrailSeek/src/components/EventsTab.js
+++ client/TrailSeek/src/components/EventsTab.js
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, View } from "react-native";
 import { Text, Button, List, ListItem, Body } from "native-base";
 import { useNavigation } from "@react-navigation/native";
+import trailSeek from "../api/trailSeek";
+
 import ColorConstants from "../util/ColorConstants";
 
 const EventsTab = ({ trailData }) => {
   // const [events] = useState([])
+  const [events, setEvents] = useState([]);
 
-  const list = [
-    {
-      name: "Event0",
-      subtitle: "Location",
-    },
-    {
-      name: "Event1",
-      subtitle: "Location",
-    },
-  ];
+  useEffect(() => {
+    const getEvents = async () => {
+      try {
+        const results = await trailSeek.get(`/trails/${trailData._id}/events`); // need to send :trailId
+        console.log(results);
+        setEvents(results.data);
+      } catch (e) {
+        console.log(e);
+        ToastAlert(e.message);
+      }
+    };
+    getEvents();
+  }, []);
+
+  // const list = [
+  //   {
+  //     name: "Event0",
+  //     subtitle: "Location",
+  //   },
+  //   {
+  //     name: "Event1",
+  //     subtitle: "Location",
+  //   },
+  // ];
 
   const navigation = useNavigation();
   return (
@@ -40,21 +57,20 @@ const EventsTab = ({ trailData }) => {
         <Text>Create Event</Text>
       </Button>
       <List>
-        {list.map((l, i) => (
+        {events.map((l, i) => (
           //ViewNavigation to be changes part of a different stack in navigation, maybe use navigation reset TBD
           <ListItem
             key={i}
-            bottomDivider
             onPress={() => {
               navigation.navigate("EventFlow", { screen: "ViewEvent" });
             }}
-            s
+            noIndent
             style={{ backgroundColor: ColorConstants.LGreen }}
           >
             <Body>
-              <Text style={styles.listText}>{l.name}</Text>
+              <Text style={styles.listText}>{l.title}</Text>
               <Text note style={styles.listText}>
-                {l.subtitle}
+                {l.description}
               </Text>
             </Body>
           </ListItem>
