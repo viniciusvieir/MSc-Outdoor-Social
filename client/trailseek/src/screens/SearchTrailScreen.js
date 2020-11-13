dab3936b04e0e8f35c44c6d2550c5e9ab67d59df
Vinicius <vini.vieir@gmail.com>
2020-11-13 19:28:48 -0300

    redesigned search trail screen

--- client/trailseek/src/screens/SearchTrailScreen.js
+++ client/trailseek/src/screens/SearchTrailScreen.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react";
-import { StyleSheet, ScrollView } from "react-native";
-import { SearchBar } from "react-native-elements";
-import { useDispatch, useSelector } from "react-redux";
-import { unwrapResult } from "@reduxjs/toolkit";
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, ScrollView, Platform } from 'react-native'
+import { SearchBar } from 'react-native-elements'
+import { useDispatch, useSelector } from 'react-redux'
+import { unwrapResult } from '@reduxjs/toolkit'
 import {
   Container,
   Content,
@@ -11,205 +11,223 @@ import {
   Header,
   Title,
   Body,
-} from "native-base";
+  View,
+} from 'native-base'
 
-import LoadSpinner from "../components/LoadSpinner";
-import ToastAlert from "../components/ToastAlert";
-import TrailCard from "../components/TrailCards";
-import CONSTANTS from "../util/Constants";
-import ColorConstants from "../util/ColorConstants";
-import { fetchTrailsByQuery } from "../app/trailSlice";
-import { getLocation, fetchUserData } from "../app/userSlice";
+import LoadSpinner from '../components/LoadSpinner'
+import ToastAlert from '../components/ToastAlert'
+import TrailCard from '../components/TrailCards'
+import CONSTANTS from '../util/Constants'
+import ColorConstants from '../util/ColorConstants'
+import { fetchTrailsByQuery } from '../app/trailSlice'
+import { getLocation, fetchUserData } from '../app/userSlice'
+import Constants from '../util/Constants'
+import TrailFilter from '../components/TrailFilter'
 
 const SearchTrailScreen = ({ navigation }) => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
-  const [searchTerm, setSearchTerm] = useState("");
-  const trailStatus = useSelector((state) => state.trails.status);
-  const error = useSelector((state) => state.trails.error);
-  const user = useSelector((state) => state.user.profile.name);
-  const isAuth = useSelector((state) => state.user.isAuth);
-  const locationStatus = useSelector((state) => state.user.userLocation.status);
+  const [searchTerm, setSearchTerm] = useState('')
+  const trailStatus = useSelector((state) => state.trails.status)
+  const error = useSelector((state) => state.trails.error)
+  const user = useSelector((state) => state.user.profile.name)
+  const isAuth = useSelector((state) => state.user.isAuth)
+  const locationStatus = useSelector((state) => state.user.userLocation.status)
   // const filteredTrails = useSelector(
   //   (state) => state.trails.filteredTrails.data
   // );
-  const [trails, setTrails] = useState([]);
+  const [trails, setTrails] = useState([])
 
   const easyParams = {
-    title: "Easy Trails",
+    title: 'Easy Trails',
     query: {
-      difficulty: "Easy",
+      difficulty: 'Easy',
       length_km: { $lt: 5 },
     },
-  };
+  }
 
   const bestParams = {
-    title: "Best Rated",
+    title: 'Best Rated',
     query: {
       avg_rating: { $gt: 4 },
     },
-  };
+  }
 
   const nearMe = {
-    title: "Near You",
+    title: 'Near You',
     location: true,
-  };
+  }
 
   let searchParam = {
-    titel: "Search Results",
+    titel: 'Search Results',
     query: {
       $text: {
         $search: `${searchTerm}`,
       },
     },
-  };
-  const [filter, setFilter] = useState(easyParams);
+  }
+  const [filter, setFilter] = useState(easyParams)
 
   let content,
-    spinner = true;
+    spinner = true
 
   const getTrailsByQuery = async ({ location = false, query, skip = 0 }) => {
     try {
-      let gpsLoc;
+      let gpsLoc
       if (location) {
         try {
-          gpsLoc = await dispatch(getLocation());
+          gpsLoc = await dispatch(getLocation())
         } catch (e) {
-          ToastAlert(e.message);
+          ToastAlert(e.message)
         }
       }
       const results = await dispatch(
         fetchTrailsByQuery({ query, location, skip })
-      );
-      const uResult = unwrapResult(results);
-      setTrails(uResult.response);
+      )
+      const uResult = unwrapResult(results)
+      setTrails(uResult.response)
     } catch (e) {
-      ToastAlert(e.message);
-      ToastAlert(error);
+      ToastAlert(e.message)
+      ToastAlert(error)
     }
-  };
+  }
+
   const getUserData = async () => {
     try {
-      const response = await dispatch(fetchUserData());
+      const response = await dispatch(fetchUserData())
     } catch (e) {
-      ToastAlert(e.message);
+      ToastAlert(e.message)
     }
-  };
+  }
 
   //Initial Trail Fetch
   useEffect(() => {
     getTrailsByQuery({
       query: filter?.query,
       location: filter?.location,
-    });
+    })
     if (isAuth) {
-      getUserData();
+      getUserData()
     }
-    setSearchTerm("");
-  }, []);
+    setSearchTerm('')
+  }, [])
 
   if (
     trailStatus === CONSTANTS.LOADING ||
     locationStatus === CONSTANTS.LOADING
   ) {
-    spinner = true;
+    spinner = true
   } else if (trailStatus === CONSTANTS.FAILED) {
-    spinner = false;
-    ToastAlert(error);
-    content = <Text>{error}</Text>;
+    spinner = false
+    ToastAlert(error)
+    content = <Text>{error}</Text>
   } else if (
     trailStatus === CONSTANTS.SUCCESS ||
     locationStatus === CONSTANTS.LOADING
   ) {
-    spinner = false;
+    spinner = false
   }
 
   return (
     <Container
-      style={{ backgroundColor: ColorConstants.LGreen, flex: 1 }}
+      style={{ backgroundColor: ColorConstants.primary, flex: 1 }}
       contentContainerStyle={{ flex: 1 }}
     >
-      <Header transparent androidStatusBarColor="#ffffff00">
+      <Header transparent androidStatusBarColor='#ffffff00'>
         <Body>
           <Title
             style={{
               color: ColorConstants.White,
-              fontWeight: "bold",
+              fontWeight: 'bold',
               fontSize: 40,
             }}
           >
-            {" "}
-            Hi! {user ? user : "User"}
+            {' '}
+            Hi {user ? user : 'User'}
           </Title>
         </Body>
       </Header>
+
       <LoadSpinner visible={spinner} />
+
       <SearchBar
-        containerStyle={styles.searchbar}
-        placeholder="Search"
+        containerStyle={{
+          borderRadius: 22,
+          marginHorizontal: Constants.POINTS.marginHorizontal,
+          height: 44,
+        }}
+        placeholder='Search'
         value={searchTerm}
         onChangeText={(text) => {
-          setSearchTerm(text);
+          setSearchTerm(text)
         }}
-        inputContainerStyle={{ height: 35, marginLeft: 3 }}
-        autoCapitalize="none"
-        platform="android"
-        autoCompleteType="name"
+        inputContainerStyle={{ height: 29, marginLeft: 2 }}
+        autoCapitalize='none'
+        platform='android'
+        autoCompleteType='name'
         enablesReturnKeyAutomatically
         onSubmitEditing={() => {
-          navigation.navigate("ListTrail", { query: searchParam.query });
+          navigation.navigate('ListTrail', { query: searchParam.query })
         }}
       />
-      <ScrollView
-        horizontal
-        style={{ marginTop: 10, maxHeight: 50, marginLeft: 10 }}
+
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          marginTop: 10,
+          maxHeight: 44,
+          marginHorizontal: 40,
+          marginBottom: 10,
+        }}
       >
-        <Button
-          rounded
-          onPress={() => {
-            setFilter(bestParams);
+        <TrailFilter
+          title={bestParams.title}
+          active={filter.title === bestParams.title}
+          action={() => {
+            setFilter(bestParams)
+            setTrails([])
             getTrailsByQuery({
               query: filter?.query,
               location: filter?.location,
-            });
+            })
           }}
-          style={styles.filterButtons}
-        >
-          <Text style={styles.filterButtonsText}>Best Rated</Text>
-        </Button>
-        <Button
-          rounded
-          style={styles.filterButtons}
-          onPress={() => {
-            setFilter(easyParams);
+        />
+        <TrailFilter
+          title={easyParams.title}
+          active={filter.title === easyParams.title}
+          action={() => {
+            setFilter(easyParams)
+            setTrails([])
             getTrailsByQuery({
               query: filter?.query,
               location: filter?.location,
-            });
+            })
           }}
-        >
-          <Text style={styles.filterButtonsText}>Easy Trails</Text>
-        </Button>
-        <Button
-          rounded
-          style={styles.filterButtons}
-          onPress={() => {
-            setFilter(nearMe);
+        />
+        <TrailFilter
+          title={nearMe.title}
+          active={filter.title === nearMe.title}
+          action={() => {
+            setFilter(nearMe)
+            setTrails([])
             getTrailsByQuery({
               query: filter?.query,
               location: filter?.location,
-            });
+            })
           }}
-        >
-          <Text style={styles.filterButtonsText}>Near You</Text>
-        </Button>
-      </ScrollView>
-      <Content
-        style={{ backgroundColor: ColorConstants.LGreen, flex: 1 }}
+        />
+      </View>
+
+      <View
+        style={{
+          backgroundColor: ColorConstants.DWhite,
+          flex: 1,
+        }}
         contentContainerStyle={{ flex: 1 }}
       >
         <TrailCard
-          title={filter.title}
           trails={trails}
           filter={filter}
           // fetchMoreData={() =>
@@ -220,24 +238,22 @@ const SearchTrailScreen = ({ navigation }) => {
           //   })
           // }
         />
-      </Content>
+      </View>
     </Container>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
-  searchbar: {
-    borderRadius: 20,
-    marginHorizontal: 10,
-    height: 50,
-  },
   filterButtons: {
+    borderColor: '#a1a1a1',
+    borderWidth: 1,
     marginHorizontal: 3,
-    backgroundColor: ColorConstants.Yellow,
+    height: 44,
+    backgroundColor: ColorConstants.secondayLight,
   },
   filterButtonsText: {
     color: ColorConstants.Black2,
   },
-});
+})
 
-export default SearchTrailScreen;
+export default SearchTrailScreen
