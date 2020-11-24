9ac554b618984fcff9e7d4e05f077c6f132532ad
Vinicius <vini.vieir@gmail.com>
2020-11-24 16:17:42 -0300

    adding trail county to trail

--- server/src/controllers/trail.js
+++ server/src/controllers/trail.js
@@ -112,6 +112,76 @@ class TrailController {
     }
   }
 
+  async rateTrail(req, res) {
+    const errors = validationResult(req)
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() })
+
+    const { id: userId } = req.context
+    const { id: trailId } = req.params
+    const { rating } = req.body
+
+    const trail = await Trail.findById(trailId).select('ratings')
+  }
+
+  // FIX
+  async trailsFix(req, res) {
+    const trails = require('../../../trails.json')
+
+    const trailsValidated = trails.map((trail) => {
+      const currentPath = trail.geoLoc.coordinates[0]
+
+      const path = currentPath.map((coordinates) => {
+        return {
+          type: 'Point',
+          coordinates: [coordinates[1], coordinates[0]],
+          elevation: coordinates[2],
+        }
+      })
+
+      let estimate_time_min = null
+      if (trail.estimateTime.includes(' h ')) {
+        const items = trail.estimateTime.split(' h ')
+        const hours = isNaN(parseInt(items[0])) ? 0 : parseInt(items[0])
+        const min = isNaN(parseInt(items[1].replace(' m')))
+          ? 0
+          : parseInt(items[1].replace(' m'))
+        estimate_time_min = hours * 60 + min
+      } else if (trail.estimateTime.includes(' m')) {
+        const min = parseInt(trail.estimateTime.replace(' m'))
+        estimate_time_min = min
+      }
+
+      if (isNaN(estimate_time_min)) {
+        console.log(trail)
+      }
+
+      return {
+        path,
+        id: trail.id,
+        name: trail.name,
+        location: trail.location,
+        county: trail.county,
+        difficulty: trail.difficulty.toLowerCase() || 'unknown',
+        length_km: trail.length_km,
+        description: trail.description,
+        activity_type: trail.activityType,
+        estimate_time_min: estimate_time_min || -1,
+        elevation_gain_ft: trail.elevationGain_ft,
+        no_of_ratings: trail.no_of_ratings,
+        avg_rating: trail.avgRating,
+        img_url: trail.img_url,
+        start: path[0],
+        end: path[path.length - 1],
+        bbox: trail.geoLoc.bbox,
+      }
+    })
+
+    await Trail.insertMany(trailsValidated)
+
+    res.json({ success: 123 })
+  }
+
   // VALIDATION
   get validators() {
     return {
