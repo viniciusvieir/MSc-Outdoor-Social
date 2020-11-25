d3395df8210b25e10eeaac55e5c8f23a13a442d5
Puja Verma <71330096+PujaVerma-06@users.noreply.github.com>
2020-11-25 19:36:18 +0000

    added county feature

--- Recommendation/Item_content.py
+++ Recommendation/Item_content.py
@@ -9,25 +9,31 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 import pymongo
+from dotenv import load_dotenv
+from pathlib import Path
+import os
 import sys
 
 class itemContentSimilarity:
     
-    client = pymongo.MongoClient('#####')
-
-    db = client['trailseek']
- 
-    trails_col = db['trail_devs']
     
-    hike_ids = ""
-    
+    hike_ids = "" 
     
     def __init__(self, user_clicked_id):
         self.user_clicked_id = user_clicked_id
+        
+        load_dotenv(dotenv_path = Path('/.env'))
+        
+        mongo_url = os.getenv('MONGO_URL')
+        client = pymongo.MongoClient(mongo_url)
+        
+        self.db = client['trailseek']
+        
 
     def cosSimilarity(self,hike_idx, df, indexed_hike_id, n):
         hike = df.iloc[hike_idx]
         cs = cosine_similarity(np.array(hike).reshape(1, -1), df)
+        #print(np.argsort(cs)[0][-(n+1):][::-1])
         rec_index = np.argsort(cs)[0][-(n+1):][::-1][1:]
         #print(rec_index)
         recommendations = []
@@ -38,13 +44,13 @@ class itemContentSimilarity:
         
         return recommendations
 
-    def fetchDB(self,trails_col):
-        
-        trails_data = trails_col.find({},{"_id":1, "difficulty": 1, "activity_type": 1, "no_of_ratings": 1, "avg_rating": 1, "estimate_time_min": 1, "length_km": 1})
+    def fetchDB(self):
+        trails_col = self.db['trails']
+        trails_data = trails_col.find({},{"_id":1, "difficulty": 1, "activity_type": 1, "no_of_ratings": 1, "avg_rating": 1, "estimate_time_min": 1, "length_km": 1, "county":1})
         df = pd.DataFrame(list(trails_data))
                       
         df['estimate_time_min'] = df['estimate_time_min'].fillna(0)
-        df = pd.get_dummies(df, columns=["difficulty", "activity_type"])      
+        df = pd.get_dummies(df, columns=["difficulty", "activity_type", "county"])      
         
         df['ratings_score'] = round((df['avg_rating']*df['no_of_ratings'])/(df['avg_rating']+df['no_of_ratings']),2)
         df['ratings_score'] = df['ratings_score'].fillna(0)
@@ -67,7 +73,7 @@ if __name__ == '__main__':
         print("Error Occured: Cause 'No Input'")
         raise
     
-    df = ITCont.fetchDB(ITCont.trails_col)
+    df = ITCont.fetchDB()
     
     try:
         for idx, id in enumerate(ITCont.hike_ids):
