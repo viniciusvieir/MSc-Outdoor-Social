e24a2b4cba89d2ac35e183142e667ffb06125b05
Vinicius <vini.vieir@gmail.com>
2020-11-26 15:04:02 -0300

    added socket.io index file and package

---
+++ server/socket/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+
+require('../src/databases')
+
+const app = require('express')()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+})
+
+const port = process.env.SOCKET_PORT
+http.listen(port, () => {
+  console.log(`listening on *:${port}`)
+})
