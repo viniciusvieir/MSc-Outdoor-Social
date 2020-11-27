e8f5a0940cfaeb0577fb7dc4947ecc3643b83d2d
Vinicius <vini.vieir@gmail.com>
2020-11-27 16:23:21 -0300

    feedback module

--- server/socket/location.js
+++ server/socket/location.js
@@ -1,27 +1,32 @@
 require('dotenv').config()
 require('../src/databases')
 
+const Event = require('../src/models/event')
+
 const app = require('express')()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 
-io.on('connection', (socket) => {
-  console.log('New socket connected', socket.id)
+io.on('connection', async (socket) => {
+  console.log('New location socket connected', socket.id)
   const { eventId, coordinates } =
     socket.handshake.headers || socket.handshake.query
   console.log(eventId, coordinates)
 
-  const room = `event-${eventId}`
+  const room = `event-location-${eventId}`
   socket.join(room)
 
   // get the location of all current users to this socket
-  const userLocations = []
+  const event = await Event.findOne({ _id: eventId }).select('participants')
+  const userLocations = event.participants.map(
+    (participant) => participant.latestLocation
+  )
   socket.emit('location:all', userLocations)
 
   socket.on('location:user', async (coordinates) => {})
 })
 
-const port = process.env.SOCKET_PORT
+const port = process.env.LOCATION_SOCKET_PORT
 http.listen(port, () => {
   console.log(`listening on *:${port}`)
 })
