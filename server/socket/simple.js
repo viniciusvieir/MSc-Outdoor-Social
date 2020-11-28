0e268057d2c7bdbfd5349c0d8ea05436e3d501d4
Ubuntu <trailseek@TrailSeekVM.fpiskdr2fuyebns4oikjy120ue.fx.internal.cloudapp.net>
2020-11-28 19:09:32 +0000

    changes simple server file

--- server/socket/simple.js
+++ server/socket/simple.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
+//const express = require('express')
+//const app = express()
 const socketio = require('socket.io')
+//const cors = require('cors')
 
-app.use(express.static(__dirname + '/client'))
-
-const expressServer = app.listen(5050)
-const io = socketio(expressServer)
+//app.use(cors())
+//app.use(express.static(__dirname + '/client'))
+const server = require('http').createServer();
+//const expressServer = app.listen(5050)
+const io = socketio(server)
 io.on('connection', (socket) => {
+	console.log('socket connected')
   socket.emit('messageFromServer', { data: 'Welcome to the socketio server' })
 })
+
+server.listen(5050)
+console.log('started')
