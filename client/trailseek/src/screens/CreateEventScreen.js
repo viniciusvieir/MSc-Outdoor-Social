edd5cc44cc5a591318ff220a57f7bd92814c80e9
neels198 <neel.salvi@gmail.com>
2020-12-09 19:07:05 +0000

    Minor Changes

--- client/trailseek/src/screens/CreateEventScreen.js
+++ client/trailseek/src/screens/CreateEventScreen.js
@@ -1,28 +1,33 @@
-import React from "react";
-import { StyleSheet } from "react-native";
-import { useDispatch } from "react-redux";
-import { Toast } from "native-base";
-import { unwrapResult } from "@reduxjs/toolkit";
+import React from 'react'
+import { StyleSheet } from 'react-native'
+import { useDispatch } from 'react-redux'
+import { Toast } from 'native-base'
+import { unwrapResult } from '@reduxjs/toolkit'
 
-import { postEvents } from "../app/eventSlice";
-import EventForms from "../components/EventForm";
+import { postEvents } from '../app/eventSlice'
+import EventForms from '../components/EventForm'
 
 const CreateEventScreen = ({ route, navigation }) => {
-  const { trailName, trailID } = route.params;
-  const dispatch = useDispatch();
+  const { trailName, trailID } = route.params
+  const dispatch = useDispatch()
   const onSubmitFunc = async (values) => {
     try {
-      const response = await dispatch(postEvents({ inputs: values, trailID }));
-      unwrapResult(response);
-      Toast.show({ text: "Event Created", type: "success", buttonText: "Ok" });
-      navigation.goBack();
+      const response = await dispatch(postEvents({ inputs: values, trailID }))
+      unwrapResult(response)
+      Toast.show({ text: 'Event Created', type: 'success', buttonText: 'Ok' })
+      navigation.goBack()
     } catch (e) {
-      ToastAlert(e.message);
+      Toast.show({
+        text: `Event Creation Error${e.message}`,
+        type: 'danger',
+        buttonText: 'Ok',
+      })
+      // ToastAlert(e.message)
     }
-  };
+  }
 
-  return <EventForms trailName={trailName} onSubmitFunc={onSubmitFunc} />;
-};
-const styles = StyleSheet.create({});
+  return <EventForms trailName={trailName} onSubmitFunc={onSubmitFunc} />
+}
+const styles = StyleSheet.create({})
 
-export default CreateEventScreen;
+export default CreateEventScreen
